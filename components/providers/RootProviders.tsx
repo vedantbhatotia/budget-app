b884db43d8609c130694957b9f6f3b87e16fd7e7
vedantbhatotia <vedantbhatotia@gmail.com>
2024-05-24 23:26:46 +0530

    defining api routes

--- components/providers/RootProviders.tsx
+++ components/providers/RootProviders.tsx
@@ -1,10 +1,13 @@
-"use client"
-
-import { ReactNode } from "react"
-import { ThemeProvider } from "next-themes"
-export default function RootProvider({children}:{children:ReactNode}){
-    return(
-        <ThemeProvider
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools"
+import React, { ReactNode } from "react";
+import { ThemeProvider } from "next-themes";
+export default function RootProvider({ children }: { children: ReactNode }) {
+  const [queryClient] = React.useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
         attribute="class"
         defaultTheme="dark"
         enableSystem
@@ -12,5 +15,7 @@ export default function RootProvider({children}:{children:ReactNode}){
       >
         {children}
       </ThemeProvider>
-    )
+      <ReactQueryDevtools initialIsOpen={false}></ReactQueryDevtools>
+    </QueryClientProvider>
+  );
 }
