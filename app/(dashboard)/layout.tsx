00c766cce16adf29ae7bd4000f400a9fd2325929
vedantbhatotia <vedantbhatotia@gmail.com>
2024-05-31 23:54:30 +0530

    de

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 import Navbar from "@/components/Navbar";
-export default function({children}:{children:ReactNode}){
+export default function Layout({children}:{children:ReactNode}){
     return(
         <div className="relative flex h-screen w-full flex-col">
       <Navbar />
